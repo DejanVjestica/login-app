6e08f358441b3b102ec5d948fd4b939a0fa34a1f
Dejan Vjestica <dvjestica@gmail.com>
2022-05-11 17:24:51 +0200

    Add useEffect and store valu to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+
+	useEffect(() => {
+		const checkIfLoggedIn = localStorage.getItem('isLoggedIn')
+
+		if (checkIfLoggedIn === 'true') {
+			setIsLoggedIn(true)
+		}
+	}, [isLoggedIn])
 
 	const loginHandler = (email: string, password: string) => {
 		// We should of course check email and password
 		// But it's just a dummy/ demo anyways
+		localStorage.setItem('isLoggedIn', 'true')
 		setIsLoggedIn(true)
 	}
 
 	const logoutHandler = () => {
 		setIsLoggedIn(false)
+		localStorage.setItem('isLoggedIn', 'false')
 	}
 
 	return (
